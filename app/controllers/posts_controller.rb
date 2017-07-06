662582a0fda546b0f3d50f7cffb19295d08a0a2e
github username <github email address>
2017-07-06 08:23:08 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,28 @@
 class PostsController < ApplicationController
+  before_action :set_blog, only: [:show]
+
+
   def index
     @posts = Post.all
   end
-  
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
+
+
+  private
+
+  def set_blog
+    @post = Post.find(params[:id])
+  end
 end
